b2ccab1d3c4f09017310ee0910bb5d2739294ee7
Daanish Mazhar <daanish@daanishmazhar.com>
2018-09-20 15:52:56 -0600

    Problem 6 finished
    No comments but working

--- assignment1/problem6_hungryKarel.py
+++ assignment1/problem6_hungryKarel.py
@@ -25,32 +25,113 @@ def comeBack():
     turn_left()
     move()
 
+
 def expandSequence():
     while (not beepers_present() and front_is_clear()):
         expandStack()
         comeBack()
 
-def findDirection():
-    if(facing_east()):
-        if not (front_is_clear()):
-            turn_left()
-    elif (facing_west()):
-        if not(front_is_clear()):
-            for i in range(3):
-                turn_left()
-    elif (facing_north()):
-        if not (right_is_clear()):
-            if(left_is_clear()):
-                turn_left()
-        elif not(left_is_clear()):
-            for i in range(3):
-                turn_left()
-def forward():
+def bottomCorner():
     while(front_is_clear()):
-        findDirection()
         move()
+
+def second():
+    turn_left()
+    while(front_is_clear()):
+        move()
+    for i in range(2):
+        turn_left()
+    move()
+    for i in range(2):
+        turn_left()
+
+def top():
+    turn_left()
+    while(front_is_clear()):
+        move()
+
+def findBeeper():
+    turn_left()
+    while(not beepers_present()):
+        move()
+    while(beepers_present()):
+        collapse()
+    while(beepers_in_bag()):
+        put_beeper()
+
+def findBeeperFinal():
+    turn_left()
+    while(not beepers_present()):
+        move()
+    while(beepers_present()):
+        pick_beeper()
+          
+
+def collapse():
+    turn_left()
+    while(beepers_present()):
+        pick_beeper()
+        move()
+        if(not front_is_clear()):
+            pick_beeper()
+            turn_left()
+
+def findBeeperSecond():
+    turn_left()
+    while(not beepers_present()):
+        move()
+    while(beepers_present()):
+        collapse()
+    turn_left()
+    while(front_is_clear()):
+        move()
+    turn_left()
+    while(beepers_in_bag()):
+        put_beeper()
+
+
+def getBack():
+    for i in range(2):
+        top()
+
+def third():
+    turn_left()
+    while(front_is_clear()):
+        move()
+    for i in range(2):
+        turn_left()
+    for i in range(2):
+        move()
+    for i in range(2):
+        turn_left()
+
+def fourth():
+    turn_left()
+    while(front_is_clear()):
+        move()
+    for i in range(2):
+        turn_left()
+    for i in range(3):
+        move()
+    for i in range(2):
+        turn_left()
 
 begin_karel_program()
 expandSequence()
-forward()
+top()
+findBeeper()
+bottomCorner()
+second()
+findBeeperSecond()
+bottomCorner()
+getBack()
+bottomCorner()
+third()
+findBeeper()
+bottomCorner()
+fourth()
+findBeeper()
+bottomCorner()
+top()
+findBeeperFinal()
 end_karel_program()
