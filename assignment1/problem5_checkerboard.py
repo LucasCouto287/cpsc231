c30d61e883977adf023e5a3b6abf5338b2944714
Daanish Mazhar <daanish@daanishmazhar.com>
2018-09-21 17:20:09 -0600

    Problem 5 Working Finally

--- assignment1/problem5_checkerboard.py
+++ assignment1/problem5_checkerboard.py
@@ -3,39 +3,73 @@
 
 from karel import *
 
-def forward():
-    move()
-    findDirection()
-
-def findDirection():
-    if(facing_east()):
-        if not (front_is_clear()):
-            turn_left()
-    elif (facing_west()):
-        if not(front_is_clear()):
-            for i in range(3):
-                turn_left()
-    elif (facing_north()):
-        if not (right_is_clear()):
-            if(left_is_clear()):
-                turn_left()
-        elif not(left_is_clear()):
-            for i in range(3):
-                turn_left()
-
 def main():
-    if not(front_is_clear()):
-            turn_left()
+	put_beeper()
+	check_wall()
+	while(front_is_clear()):
+  		beepers_east()
+  		beepers_west()
 
-    while (front_is_clear()):
-        if not (beepers_present()):
-            put_beeper()
-            forward()
+def turn_right():
+	for i in range(3):
+		turn_left()
 
-        if(front_is_clear()):
-            forward()
-            if (beepers_present()):
-                put_beeper()
+def beepers_east():
+	while(facing_east()):
+		move()
+		if(front_is_clear()):
+			move()
+			put_beeper()
+		up_east()
+
+def up_east():
+	if(not front_is_clear()):
+		if(not beepers_present()):
+			turn_left()
+			if(front_is_clear()):
+				move()
+				turn_left()
+				put_beeper()
+		else:
+			turn_left()
+			if(front_is_clear()):
+				move()
+				turn_left()
+				move()
+				put_beeper()
+
+def beepers_west():
+	while(facing_west()):
+		move()
+		if(front_is_clear()):
+			move()
+			put_beeper()
+		up_west()
+
+def up_west():
+	if(not front_is_clear()):
+		if(not beepers_present()):
+			turn_right()
+			if(front_is_clear()):
+				move()
+				turn_right()
+				put_beeper()
+		else:
+			turn_right()
+			if(front_is_clear()):
+				move()
+				turn_right()
+				move()
+				put_beeper()
+
+def check_wall():
+	if(not front_is_clear()):
+		turn_left()
+		while(front_is_clear()):
+			move()
+			if(front_is_clear()):
+				move()
+				put_beeper()
 
 begin_karel_program()
 main()
