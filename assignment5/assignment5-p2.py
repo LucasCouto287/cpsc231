e0f6b40ce8cb2da127e41abb66003139839ad0eb
Abdullah F. Khan <abdullahkhan0503@gmail.com>
2018-11-20 13:20:58 -0700

    rename assignment files
    rename the assignment files to how the TA said they should be

--- assignment5/assignment5-p2.py
+++ assignment5/assignment5-p2.py
@@ -16,6 +16,7 @@ bg = Color(255, 255, 255)
 #
 # colors = [red, green, yellow, blue, purple, orange]
 
+# all the possible game pieces
 flags = ["brazil", "canada", "china", "india", "usa", "pakistan"]
 
 # set up the canvas
@@ -32,12 +33,18 @@ grid = [[],
         [],
         [],
         []]
+centerCoordinatesX = []
+centerCoordinatesY = []
 
 # append flags to the 2D grid (while taking care of three-in-a-row cases)
 for outerIndex in range(len(grid)):
-    streak = 0
     for index in range(len(grid) - 2):
         randomFlag = str(random.choice(flags))
+        # append the indices to create a coordinates list
+        if outerIndex == 0:
+            centerCoordinatesX.append((index + 1) * 2)
+            # the y-coordinates are just the reverse of the x-coordinates with the addition of 16 & 18
+            centerCoordinatesY = [18, 16] + centerCoordinatesX[::-1]
         # append the first two flags no matter if they are equal
         if len(grid[outerIndex]) < 2:
             grid[outerIndex].append(randomFlag)
@@ -54,27 +61,48 @@ for outerIndex in range(len(grid)):
             else:
                 grid[outerIndex].append(randomFlag)
 
-print([i[index] for i in grid[::-1]])
-# for index in range(len(grid)):
-    # for innerIndex in range(len(grid[index])):
-    # print(grid[index][0])
-    # print("")
+# create a reversed version of the original grid (to check for verticals)
+# when iterated over, the original grid's flags corresponded to the wrong indices for some reason, which is why this step is crucial
+reversedGrid = grid[::-1]
+
+# replace vertical duplicates (the same flag more than two-in-a-row) in all columns with other flags
+column = 0
+for outerIndex in range(7):
+    for index in range(len(reversedGrid)):
+        # check for vertical equals starting from the third flag in the column
+        if index > 1:
+            if reversedGrid[index][column] == reversedGrid[index - 1][column] and reversedGrid[index][column] == reversedGrid[index - 2][column]:
+                flagsExcludingDuplicate = flags.copy()
+                flagsExcludingDuplicate.remove(reversedGrid[index][column])
+                reversedGrid[index][column] = random.choice(flagsExcludingDuplicate)
+                del flagsExcludingDuplicate[:]
+    column += 1
 
 pictureGrid = []
+# reverse the reversed grid to bring it back to normal
+boardGrid = reversedGrid[::-1]
 for index in range(len(grid)):
     for innerIndex in range(len(grid[index])):
-        pictureGrid.append(grid[index][innerIndex])
+        pictureGrid.append(boardGrid[index][innerIndex])
 
-def generateBoard():
+def showBoard():
     pictureIndex = 0
     for outerIndex in range(1, len(grid) + 1):
         for index in range(1, len(grid[outerIndex - 1]) + 1):
             pictureIndex += 1
             stddraw.picture(picture.Picture(pictureGrid[pictureIndex - 1] + ".png"), index * 2, outerIndex * 2)
-    # stddraw.setPenColor(random.choice(colors))
-    # stddraw.filledCircle(8, 18, 0.6)
 
-# while True:
-generateBoard()
-stddraw.show()
-    # stddraw.clear(bg)
+stddraw.setPenColor(Color(30, 30, 30))
+
+def drawNavigator():
+    if stddraw.mousePressed():
+        # get the closest center value from the list of center coordinates
+        # (find the coordinates with the minimum distance from the mouse coordinates)
+        x = min(centerCoordinatesX, key=lambda x:abs(x - stddraw.mouseX()))
+        y = min(centerCoordinatesY, key=lambda y:abs(y - stddraw.mouseY()))
+        stddraw.rectangle(x - 0.75, y - 0.75, 1.5, 1.5)
+
+while True:
+    showBoard()
+    drawNavigator()
+    stddraw.show(0)
