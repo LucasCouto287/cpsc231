516359eb679cf3c2a97e6a38de9057f1f560d9bf
Abdullah F. Khan <abdullahkhan0503@gmail.com>
2018-11-22 21:43:23 -0700

    assigment 5: swapping functionality in progress

--- assignment5/assignment5-p2.py
+++ assignment5/assignment5-p2.py
@@ -2,22 +2,14 @@
 # Abdullah Khan - 30074457 - Tutorial 1
 # Afnan Imran Chaudhry - 30077054 - Tutorial 10
 
-import random, stddraw, picture
+import random, stddraw, picture, math
+from itertools import groupby
 from color import Color
 
-bg = Color(255, 255, 255)
-
-# red = Color(255, 55, 75)
-# green = Color(55, 255, 75)
-# yellow = Color(245, 245, 55)
-# blue = Color(50, 175, 255)
-# purple = Color(145, 55, 255)
-# orange = Color(255, 155, 70)
-#
-# colors = [red, green, yellow, blue, purple, orange]
+bg = Color(35, 35, 35)
 
 # all the possible game pieces
-flags = ["brazil", "canada", "china", "india", "usa", "pakistan"]
+shapes = ["triangle", "circle", "parallelogram", "diamond", "star", "pentagon"]
 
 # set up the canvas
 stddraw.setXscale(0.0, 16.0)
@@ -36,73 +28,128 @@ grid = [[],
 centerCoordinatesX = []
 centerCoordinatesY = []
 
-# append flags to the 2D grid (while taking care of three-in-a-row cases)
+# append shapes to the 2D grid (while taking care of three-in-a-row cases)
 for outerIndex in range(len(grid)):
     for index in range(len(grid) - 2):
-        randomFlag = str(random.choice(flags))
+        randomShape = str(random.choice(shapes))
         # append the indices to create a coordinates list
         if outerIndex == 0:
             centerCoordinatesX.append((index + 1) * 2)
             # the y-coordinates are just the reverse of the x-coordinates with the addition of 16 & 18
-            centerCoordinatesY = [18, 16] + centerCoordinatesX[::-1]
-        # append the first two flags no matter if they are equal
+            centerCoordinatesY = centerCoordinatesX[::-1] + [16, 18]
+        # append the first two shapes no matter if they are equal
         if len(grid[outerIndex]) < 2:
-            grid[outerIndex].append(randomFlag)
-        # check for equals now, starting from the third flag (horizontal only)
+            grid[outerIndex].append(randomShape)
+        # check for equals now, starting from the third hape (horizontal only)
         elif len(grid[outerIndex]) >= 2:
-            if randomFlag == grid[outerIndex][index - 1] and randomFlag == grid[outerIndex][index - 2]:
-                # make a copy of the flags list and then remove the duplicate flag from the new list
-                # replace the duplicate flag on the grid with a random flag from the new list, then empty the new list
-                flagsExcludingDuplicate = flags.copy()
-                flagsExcludingDuplicate.remove(randomFlag)
-                grid[outerIndex].append(random.choice(flagsExcludingDuplicate))
-                del flagsExcludingDuplicate[:]
-                # print("equal, flag = {}".format(randomFlag))
+            if randomShape == grid[outerIndex][index - 1] and randomShape == grid[outerIndex][index - 2]:
+                # make a copy of the shapes list and then remove the duplicate hape from the new list
+                # replace the duplicate hape on the grid with a random hape from the new list, then empty the new list
+                shapesExcludingDuplicate = shapes.copy()
+                shapesExcludingDuplicate.remove(randomShape)
+                grid[outerIndex].append(random.choice(shapesExcludingDuplicate))
+                del shapesExcludingDuplicate[:]
             else:
-                grid[outerIndex].append(randomFlag)
+                grid[outerIndex].append(randomShape)
 
 # create a reversed version of the original grid (to check for verticals)
-# when iterated over, the original grid's flags corresponded to the wrong indices for some reason, which is why this step is crucial
+# when iterated over, the original grid's shapes corresponded to the wrong indices for some reason, which is why this step is crucial
 reversedGrid = grid[::-1]
 
-# replace vertical duplicates (the same flag more than two-in-a-row) in all columns with other flags
+# replace vertical duplicates (the same hape more than two-in-a-row) in all columns with other shapes
 column = 0
 for outerIndex in range(7):
     for index in range(len(reversedGrid)):
-        # check for vertical equals starting from the third flag in the column
+        # check for vertical equals starting from the third hape in the column
         if index > 1:
             if reversedGrid[index][column] == reversedGrid[index - 1][column] and reversedGrid[index][column] == reversedGrid[index - 2][column]:
-                flagsExcludingDuplicate = flags.copy()
-                flagsExcludingDuplicate.remove(reversedGrid[index][column])
-                reversedGrid[index][column] = random.choice(flagsExcludingDuplicate)
-                del flagsExcludingDuplicate[:]
+                shapesExcludingDuplicate = shapes.copy()
+                shapesExcludingDuplicate.remove(reversedGrid[index][column])
+                reversedGrid[index][column] = random.choice(shapesExcludingDuplicate)
+                del shapesExcludingDuplicate[:]
     column += 1
 
-pictureGrid = []
+stddraw.setPenColor(Color(220, 220, 220))
+
+rectCoordinates = []
+indices = []
+shapesClickedOn = []
+def drawNavigator(mouseX, mouseY):
+    # if stddraw.mousePressed():
+    # get the closest center value from the list of center coordinates
+    # aka finding the coordinates with the minimum distance from the mouse coordinates
+    x = min(centerCoordinatesX, key=lambda x:abs(x - mouseX))
+    y = min(centerCoordinatesY, key=lambda y:abs(y - mouseY))
+    rectCoordinates.append((x - 0.875, y - 0.875))
+    stddraw.rectangle(rectCoordinates[len(rectCoordinates) - 1][0], rectCoordinates[len(rectCoordinates) - 1][1], 1.75, 1.75)
+
+    # to get the indices of the clicked shape
+    indices.append((int(abs((y / 2) - 9)), int(x / 2) - 1))
+    # get the shape using the indices above
+    shapesClickedOn.append(reversedGrid[indices[len(indices) - 1][0]][indices[len(indices) - 1][1]])
+    swapShapes()
+
+def swapShapes():
+    global boardGrid
+    # print(indices)
+    for index in range(len(indices) - 1):
+        if index % 2 == 0:
+            # get the last two elements in the indices list (the two indices which need to be swapped)
+            swapIndices = indices[-2:]
+            # check if the shapes that need to be swapped are adjacent before swapping
+            if (indices[index][0] == indices[index - 1][0]) or (indices):
+                reversedGrid[swapIndices[index - 1][0]][swapIndices[index - 1][1]], reversedGrid[swapIndices[index][0]][swapIndices[index][1]] = reversedGrid[swapIndices[index][0]][swapIndices[index][1]], reversedGrid[swapIndices[index - 1][0]][swapIndices[index - 1][1]]
+                boardGrid = reversedGrid[::-1]
+                del swapIndices[:]
+    showBoard(boardGrid)
+    checkForEquals()
+
 # reverse the reversed grid to bring it back to normal
 boardGrid = reversedGrid[::-1]
-for index in range(len(grid)):
-    for innerIndex in range(len(grid[index])):
-        pictureGrid.append(boardGrid[index][innerIndex])
 
-def showBoard():
+reversedBoardGrid = boardGrid[::-1]
+def checkForEquals():
+    verticalColumn = 0
+    for outerIndex in range(len(reversedBoardGrid)):
+        consecutiveTaffies = [sum(1 for _ in group) for _, group in groupby(reversedBoardGrid[outerIndex])]
+        for index in range(len(consecutiveTaffies)):
+            if consecutiveTaffies[index] >= 3:
+                print("equal (horizontal), at indices", outerIndex + 1, index + 1)
+    print("")
+        # for index in range(len(reversedBoardGrid[outerIndex])):
+        # currentShape = reversedBoardGrid[outerIndex][index]
+            # if currentShape == reversedBoardGrid[outerIndex][index - 1] and currentShape == reversedBoardGrid[outerIndex][index - 2]:
+                # print("equal (horizontal)")
+    for outerIndex in range(7):
+        for index in range(len(reversedBoardGrid)):
+            if index > 1:
+                if (reversedBoardGrid[index][verticalColumn] == reversedBoardGrid[index - 1][verticalColumn] and
+                reversedBoardGrid[index][verticalColumn] == reversedBoardGrid[index - 2][verticalColumn]):
+                    print("equal (vertical)")
+            elif index > 2:
+                if (reversedBoardGrid[index][verticalColumn] == reversedBoardGrid[index - 1][verticalColumn] and
+                reversedBoardGrid[index][verticalColumn] == reversedBoardGrid[index - 2][verticalColumn] and
+                reversedBoardGrid[index][verticalColumn] == reversedBoardGrid[index - 2][verticalColumn] == reversedBoardGrid[index - 3][verticalColumn]):
+                    print("equal (vertical)")
+        verticalColumn += 1
+
+def showBoard(board):
+    pictureGrid = []
+    for index in range(len(grid)):
+        for innerIndex in range(len(grid[index])):
+            pictureGrid.append(board[index][innerIndex])
     pictureIndex = 0
     for outerIndex in range(1, len(grid) + 1):
         for index in range(1, len(grid[outerIndex - 1]) + 1):
             pictureIndex += 1
             stddraw.picture(picture.Picture(pictureGrid[pictureIndex - 1] + ".png"), index * 2, outerIndex * 2)
-
-stddraw.setPenColor(Color(30, 30, 30))
-
-def drawNavigator():
-    if stddraw.mousePressed():
-        # get the closest center value from the list of center coordinates
-        # (find the coordinates with the minimum distance from the mouse coordinates)
-        x = min(centerCoordinatesX, key=lambda x:abs(x - stddraw.mouseX()))
-        y = min(centerCoordinatesY, key=lambda y:abs(y - stddraw.mouseY()))
-        stddraw.rectangle(x - 0.75, y - 0.75, 1.5, 1.5)
-
+print(boardGrid[::-1])
+stddraw.clear(bg)
 while True:
-    showBoard()
-    drawNavigator()
-    stddraw.show(0)
+    showBoard(boardGrid)
+    if stddraw.mousePressed():
+        if len(rectCoordinates) >= 1:
+            showBoard(boardGrid)
+            stddraw.clear(bg)
+        drawNavigator(stddraw.mouseX(), stddraw.mouseY())
+    stddraw.show(0.0)
